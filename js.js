7bd51e4e61009a732be502e1cea5430084e3d07a
R F <15.robis@gmail.com>
2022-03-15 13:37:19 +0200

    egzamino dalies pildymas

---
+++ js.js
@@ -0,0 +1,6 @@
+// function toogleMenu() {
+//     const toogleMenu = document.querySelector("toogleMenu");
+//     const toogleMenu = document.querySelector("navigation");
+//     toogleMenu.classList.toggle("active");
+//     navigation.classList.toggle("active");
+// }
